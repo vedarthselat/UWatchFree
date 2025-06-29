58c73281538a6b2bf298ebb67ece35e966a11655
Vedarth Kumar Selat <vedarthselat@trentu.ca>
2025-06-29 16:15:53 +0530

    Added env

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const connectToMongo = require("./db");
 const express = require("express");
 const cors = require("cors");
@@ -5,6 +6,8 @@ const mongoose = require("mongoose");
 connectToMongo();
 
 const app = express();
+
+
 app.use(cors());
 app.use(express.json());
 app.use(express.json({ limit: "10mb" }));
