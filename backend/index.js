6ebca75fc8d6674b9b8bd95c3618dd2821799ebb
Vedarth Kumar Selat <vedarthselat@trentu.ca>
2026-04-30 02:55:30 -0400

    Added netlify in CORS

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,10 @@ const app = express();
 
 // Middleware setup
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://uwatchfree1.netlify.app"
+  ],
   credentials: true
 }));
 
